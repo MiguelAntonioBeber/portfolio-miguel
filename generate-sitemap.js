404e56022665e15bf0ef5a78f6a1c0c0cf5df554
Miguel Antonio Beber <112721503+MiguelAntonioBeber@users.noreply.github.com>
2025-04-19 21:46:08 -0300

    Responsividade para tablets

---
+++ generate-sitemap.js
@@ -0,0 +1,17 @@
+import { SitemapStream, streamToPromise } from 'sitemap';
+import { createWriteStream } from 'fs';
+
+const sitemap = new SitemapStream({ hostname: 'https://miguelantoniobeber.github.io/portfolio-miguel-beber/' });
+
+const writeStream = createWriteStream('./public/sitemap.xml');
+
+sitemap.pipe(writeStream);
+
+sitemap.write({ url: '/portfolio-miguel-beber//', changefreq: 'monthly', priority: 1.0 });
+sitemap.write({ url: 'portfolio-miguel-beber/#/contato', changefreq: 'monthly', priority: 0.8 });
+
+sitemap.end();
+
+streamToPromise(sitemap).then(() => {
+  console.log('✅ Sitemap gerado com sucesso!');
+});
